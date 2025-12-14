301dc3b35c76b4707074ed36eb230657b6727bda
Hapid Triansyah <hapid.triansyah01@gmail.com>
2025-12-14 19:38:55 +0700

    fix test

--- tests/sum-test.js
+++
@@ -1,6 +0,0 @@
-// tests/sum.test.js
-const sum = require("../src/sum");
-
-test("menjumlahkan 1 + 2 menjadi 3", () => {
-  expect(sum(1, 2)).toBe(3);
-});
--- tests/sum.test.js
+++ tests/sum.test.js
@@ -2,5 +2,5 @@
 const sum = require("../src/sum");
 
 test("menjumlahkan 1 + 2 menjadi 3", () => {
-  expect(sum(1, 2)).toBe(3);
+  expect(sum(9, 2)).toBe(11);
 });
